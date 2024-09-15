80531b0c3a07a4538a900740f9d6b7ec6ed3e660
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-09-15 14:38:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,18 +53,22 @@ def manual_pit_strategy(total_laps, lap_length_km, average_speed_kmh, pit_stop_t
     pit_stops = 0
     lap_data = []
 
+    pit_laps = sorted(set(manual_pit_laps))  # Menghapus duplikat dan mengurutkan lap
+    lap_idx = 0  # Indeks lap yang sedang dihitung
+
     for lap in range(1, total_laps + 1):
-        if lap in manual_pit_laps:
+        if lap_idx < len(pit_laps) and lap == pit_laps[lap_idx]:
             time_so_far += lap_time(tire_wear, lap_length_km, average_speed_kmh) + pit_stop_time
             tire_wear = 0  # Reset keausan ban setelah pit stop
             pit_stops += 1
+            lap_idx += 1
         else:
             time_so_far += lap_time(tire_wear, lap_length_km, average_speed_kmh)
             tire_wear += wear_increase_per_lap
 
-        lap_data.append((lap_time(tire_wear, lap_length_km, average_speed_kmh), pit_stops, tire_wear))
+        lap_data.append((lap, time_so_far, pit_stops, tire_wear))
 
-    return time_so_far, lap_data, manual_pit_laps
+    return time_so_far, lap_data, pit_laps
 
 # Membaca data sirkuit dari CSV
 df_sirkuit = pd.read_csv('data_sirkuit.csv')
